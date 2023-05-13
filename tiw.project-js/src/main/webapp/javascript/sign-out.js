7e5257e6c47ac0d045dd314dacb09a7180b30f15
Mattia Campana <campana.mattia@outlook.com>
2023-05-13 14:42:50 +0200

    sign out js

---
+++ tiw.project-js/src/main/webapp/javascript/sign-out.js
@@ -0,0 +1,16 @@
+(function(){
+    document.getElementById("sign-out").addEventListener("submit", (event) =>{
+        event.preventDefault();
+        makeCall("POST", "SignOut", null, function (res) {
+            if(res.readyState === XMLHttpRequest.DONE){
+                let message = res.responseText;
+                if(res.status === 200){
+                    sessionStorage.removeItem("userName");
+                    window.location.href = "../sign-in.html";
+                }else{
+                    document.getElementById("error").textContent = message;
+                }
+            }
+        });
+    });
+})();
