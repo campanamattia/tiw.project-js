381e95dc76672a9faf2cb70a628d8f7bee8cd2bd
FraRita <francescorita2709@gmail.com>
2023-05-13 11:22:14 +0200

    Cloned html version java sources

---
+++ tiw.project-js/src/main/java/it/polimi/tiw/playlist/filters/PlaylistChecker.java
@@ -0,0 +1,33 @@
+package it.polimi.tiw.playlist.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpSession;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter( urlPatterns = {"/Playlist"})
+public class PlaylistChecker implements Filter {
+	
+	//checks that the page is accessed through the button in the Home page; in case it is not so, redirect to the Home page
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String HomePath = req.getServletContext().getContextPath() + "/Home?playlistListError=Playlist+not+found";
+
+		HttpSession s = req.getSession();
+		if (s.isNew() || request.getParameter("playlistName") == null || request.getParameter("playlistName").isEmpty()) {
+			res.sendRedirect(HomePath);
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+	
+}
