ffc6dc0dca7c01fc5351a82d46002cdc438a2ec3
Mattia Campana <campana.mattia@outlook.com>
2023-05-15 11:27:37 +0200

    refactored sign-in in filter

--- tiw.project-js/src/main/webapp/javascript/filter.js
+++ tiw.project-js/src/main/webapp/javascript/filter.js
@@ -1,7 +1,7 @@
 
 window.addEventListener("load" , (event) => {
     if(sessionStorage.getItem("userName") == null){
-        window.location.href = "../sign-in.html";
+        window.location.href = "sign-in.html";
     }else{
         render.showHomePage();
     }
